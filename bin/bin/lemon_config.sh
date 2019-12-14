8fb3801ccc1095cb2492f053b37ad8c341a10787
johngodlee <johngodlee@gmail.com>
2019-12-14 09:20:42 +0000

    Added some bar stuf

--- bin/bin/lemon_config.sh
+++ bin/bin/lemon_config.sh
@@ -4,7 +4,24 @@ Clock () {
 	date '+%Y-%m-%d %H:%M:%S'
 }
 
+Bat () {
+	BATS=$(cat /sys/class/power_supply/BAT0/status)
+	BATC=$(cat /sys/class/power_supply/BAT0/capacity)
+	if [ "$BATS" = "Charging" ]; then
+		echo "C"
+	else
+		echo "$BATC"
+	fi
+}
+
+Wifi () {
+	iw dev wlp2s0 link | grep "SSID" | awk '{print $2}'
+}
+
+sep=" | "
+
 while true; do
-	echo -e "%{r}$(Clock)"
+	printf "%s\n" "%{l}%{r}$(Wifi)$sep$(Bat)$sep$(Clock) "
 	sleep 1s
 done
+
